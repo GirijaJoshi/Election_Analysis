a69570c991217f728e570d4f11c9b504032caade
Girija Joshi <girija_aj@yahoo.com>
2020-05-26 15:29:37 -0700

    Adding the election audit code.

--- PyPollWithAllTries.py
+++ PyPollWithAllTries.py
@@ -20,6 +20,51 @@ import csv
 
 # Use the now() attribute on the datetime class to get the present time.
 now = dt.datetime.now()
+# Print the present time.
+# print("The time right now is ", now)
+
+# # Assign a variable for the file to load and the path.
+# # file_to_load = 'Resources/election_results.csv'
+# file_to_load = os.path.join("Resources", "election_results.csv")
+#
+# # Open the election results and read the file.
+# with open(file_to_load) as election_data:
+#     # To do: perform analysis.
+#     print(election_data)
+#
+# # Create a filename variable to a direct or indirect path to the file.
+# file_to_save = os.path.join("analysis", "election_analysis.txt")
+# # Using the open() function with the "w" mode we will write data to the file.
+# # Using the with statement open the file as a text file.
+# with open(file_to_save, "w") as txt_file:
+#
+#     # # Write three counties to the file.
+#     # txt_file.write("Arapahoe")
+#     # txt_file.write("Denver")
+#     # txt_file.write("Jefferson")
+#     # # output would be
+#     # # ArapahoeDenverJefferson
+#
+#     # # Write three counties to the file.
+#     # txt_file.write("Arapahoe, ")
+#     # txt_file.write("Denver, ")
+#     # txt_file.write("Jefferson")
+#     # # outout : Arapahoe, Denver, Jefferson
+#
+#     # # Write three counties to the file.
+#     # txt_file.write("Arapahoe\nDenver\nJefferson")
+#     # # output:
+#     # # Arapahoe
+#     # # Denver
+#     # # Jefferson
+#
+#     # Skills
+#     message_to_candidate = (
+#         f"Counties in the election. \n"
+#         f"------------------------------------------------------------------\n"
+#         f"Arapahoe\nDenver\nJefferson")
+#     txt_file.write(message_to_candidate)
+
 # Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources/election_results.csv")
 # Assign a variable to save the file to a path.
@@ -37,12 +82,19 @@ winning_percentage = 0
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
+    # To do: read and analyze the data here.
     # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
+    # Print each row in the CSV file.
+    # for row in file_reader:
+    #     print(row)
+    # for row in file_reader:
+    #     print(row)
+    #     print(row[0])
 
     # Print the header row.
     headers = next(file_reader)
-
+    # print(headers)
     # Print each row in the CSV file.
     for row in file_reader:
         # 2. Add to the total vote count.
@@ -67,7 +119,8 @@ for candidate in candidate_votes:
     # 3. Calculate the percentage of votes.
     vote_percentage = int(votes) / int(total_votes) * 100
     # 4. Print the candidate name and percentage of votes.
-
+    # print(f"{candidate}: received {vote_percentage:.1f}% of the vote.")
+    # Determine winning vote count and candidate
     # 1. Determine if the votes are greater than the winning count.
     if (votes > winning_count) and (vote_percentage > winning_percentage):
         # 2. If true then set winning_count = votes and winning_percent =
@@ -85,3 +138,13 @@ winning_candidate_summary = (
     f"Winning Percentage: {winning_percentage:.1f}%\n"
     f"-------------------------\n")
 print(winning_candidate_summary)
+# # 3. Print the total votes.
+# print(total_votes)
+# print(candidate_options)
+# print(candidate_votes)
+# print(f"Winning candidate: {winning_candidate}, vote count: {winning_count}, percentage: {winning_percentage:.1f}%.")
+
+
+
+
+
